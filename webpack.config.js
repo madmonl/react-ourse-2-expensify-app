1b7a1a99dee56e1967c92d80562955ff8b667f5b
Liad Madmon <madmonl@post.bgu.ac.il>
2018-08-17 14:08:03 +0300

    fixed = problem at webpack.json.js

--- webpack.config.js
+++ webpack.config.js
@@ -52,12 +52,12 @@ module.exports = (env) => {
         plugins: [
             CSSExtract,
             new webpack.DefinePlugin({
-                'process.env.FIREBASE_API_KEY' = JSON.Stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_AUTH_DOMAIN' = JSON.Stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_DATABASE_URL' = JSON.Stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_PROJECT_ID' = JSON.Stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_STORAGE_BUCKET' = JSON.Stringify(process.env.FIREBASE_API_KEY),
-                'process.env.FIREBASE_MESSAGING_SENDER_ID' = JSON.Stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_API_KEY': JSON.Stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_AUTH_DOMAIN': JSON.Stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_DATABASE_URL': JSON.Stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_PROJECT_ID': JSON.Stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_STORAGE_BUCKET': JSON.Stringify(process.env.FIREBASE_API_KEY),
+                'process.env.FIREBASE_MESSAGING_SENDER_ID': JSON.Stringify(process.env.FIREBASE_API_KEY)
             })
         ],
         devtool: isProduction ? 'source-map' : 'inline-source-map',
